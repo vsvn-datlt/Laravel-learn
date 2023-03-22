6fe6cd6c3640d816c464a365ac617130694a2ad1
datlt-vsvn <tandat.luong@veriserve-vietnam.com>
2023-03-22 09:50:35 +0700

    feat: routes parameters

--- routes/web.php
+++ routes/web.php
@@ -24,3 +24,16 @@ Route::get('/contacts', function () {
 Route::get('/contacts/create', function () {
     return "<h1>Create new contact</h1>";
 });
+
+// Routes parameters
+Route::get('/contacts/{id}', function ($id) {
+    return "Contact " . $id;
+});
+
+// Optional parameters
+Route::get('/companies/{name?}', function ($company_name = null) {
+    if ($company_name)
+        return "Company " . $company_name;
+    else
+        return "<h1>All companies</h1";
+});
