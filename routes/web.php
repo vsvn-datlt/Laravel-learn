b75c1d91cfecf95df167c9e8914599b5c29f44c1
datlt-vsvn <tandat.luong@veriserve-vietnam.com>
2023-03-22 10:16:08 +0700

    feat: named routes

--- routes/web.php
+++ routes/web.php
@@ -14,16 +14,36 @@ use Illuminate\Support\Facades\Route;
 */
 
 Route::get('/', function () {
-    return view('welcome');
+    $html_content = '
+    <!DOCTYPE html>
+    <html lang="en">
+    <head>
+        <meta charset="UTF-8">
+        <meta http-equiv="X-UA-Compatible" content="IE=edge">
+        <meta name="viewport" content="width=device-width, initial-scale=1.0">
+        <title>Laravel learn</title>
+    </head>
+    <body>
+        <h1>My Contact app</h1>
+        <a href="' . route("contacts.index") . '">All contacts</a>
+        <a href="' . route("contacts.create") . '">Add new contact</a>
+        <a href="' . route("contacts.show", 1) . '">Show contact</a>
+    </body>
+    </html>
+    ';
+    // return view('welcome');
+    return $html_content;
 });
 
 Route::get('/contacts', function () {
     return "<h1>All contacts</h1>";
-});
+})
+    ->name("contacts.index");
 
 Route::get('/contacts/create', function () {
     return "<h1>Create new contact</h1>";
-});
+})
+    ->name("contacts.create");;
 
 // Routes parameters
 Route::get('/contacts/{id}', function ($id) {
@@ -31,7 +51,7 @@ Route::get('/contacts/{id}', function ($id) {
 })
     // ->where("id", "\d+") // use regex for contraining
     ->whereNumber("id") // use build-in function for constraining
-;
+    ->name("contacts.show");;
 
 // Optional parameters
 Route::get('/companies/{name?}', function ($company_name = null) {
