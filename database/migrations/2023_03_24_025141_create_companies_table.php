ae6f7c2d520ee34c6ecfc1bfbf3074c8d202fe28
datlt-vsvn <tandat.luong@veriserve-vietnam.com>
2023-03-24 10:02:21 +0700

    feat: create first migrations

---
+++ database/migrations/2023_03_24_025141_create_companies_table.php
@@ -0,0 +1,34 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('companies', function (Blueprint $table) {
+            $table->id();
+            $table->string("name")->nullable()->comment("Name of the company");
+            $table->string("address")->nullable()->comment("Address of the company");
+            $table->string("website")->comment("Email of the company");
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('companies');
+    }
+};
