75a1066db77b4bb31c07b471b7ebd1d8ffeef25e
datlt4 <luongtandat.m0145@gmail.com>
2023-05-03 16:44:15 +0700

    feat: searching soft deleted models

--- resources/views/contacts/filter.blade.php
+++ resources/views/contacts/filter.blade.php
@@ -1,28 +1,27 @@
 <div class="row">
-    <div class="col-md-6"></div>
-    <div class="col-md-6">
+    <div class="col-md-12">
         <form>
             <div class="row">
                 <div class="col">
+                    <input type="hidden" class="trash" name="trash" value="{{ request()->query('trash') }}">
+                </div>
+                <div class="col-md-2">
                     <a href="{{ request()->fullUrlWithQuery(["trash" => false]) }}" class="btn {{ !request()->query('trash') ? 'text-primary' : 'text-secondary' }}">All</a> |
                     <a href="{{ request()->fullUrlWithQuery(["trash" => true]) }}" class="btn {{ request()->query('trash') ? 'text-primary' : 'text-secondary' }}">Trash</a>
                 </div>
-                <div class="col">
+                <div class="col-md-3">
                     {{-- @include("contacts.company_selection") --}}
                     {{-- @includeIf("contacts.company_selection") --}}
                     {{-- @includeWhen(!empty($companies), "contacts.company_selection") --}}
                     {{-- @includeUnless(empty($companies), 'contacts.company_selection') --}}
                     @includeUnless(empty($company_count), 'contacts.company_selection')
                 </div>
-                <div class="col">
+                <div class="col-md-3">
                     <div class="input-group mb-3">
-                        <input type="text" class="form-control" name="search" value="{{ request()->query('search') }}"
-                            id="search-input" placeholder="Search..." aria-label="Search..."
-                            aria-describedby="button-addon2">
+                        <input type="text" class="form-control" name="search" value="{{ request()->query('search') }}" id="search-input" placeholder="Search..." aria-label="Search..." aria-describedby="button-addon2">
                         <div class="input-group-append">
                             @if (request()->filled('search') || request()->filled('company_id'))
-                                <button class="btn btn-outline-secondary" type="button"
-                                    onclick="document.getElementById('search-input').value = '', document.getElementById('search-select').selectedIndex = 0, this.form.submit()">
+                                <button class="btn btn-outline-secondary" type="button" onclick="document.getElementById('search-input').value = '', document.getElementById('search-select').selectedIndex = 0, this.form.submit()">
                                     <i class="fa fa-refresh"></i>
                                 </button>
                             @endif
@@ -35,5 +34,4 @@
             </div>
         </form>
     </div>
-</div>
 </div>
