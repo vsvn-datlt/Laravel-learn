342f4f07e9555b3859d3d44ece1e357c80663c1d
datlt-vsvn <tandat.luong@veriserve-vietnam.com>
2023-05-17 18:14:00 +0700

    feat: protecting routes from unauthenticated users

--- routes/web.php
+++ routes/web.php
@@ -7,6 +7,7 @@ use App\Http\Controllers\TagController;
 use App\Http\Controllers\CompanyController;
 use App\Http\Controllers\WelcomeController;
 use App\Http\Controllers\ContactController;
+use App\Http\Controllers\DashboardController;
 use Illuminate\Support\Facades\Route;
 
 /*
@@ -21,54 +22,56 @@ use Illuminate\Support\Facades\Route;
 */
 
 // Route::get("/", [WelcomeController::class, "home"])
-Route::get("/", WelcomeController::class)
-    ->name("index");
+Route::get("/", WelcomeController::class)->name("index");
 
-Route::controller(ContactController::class)->prefix("contacts")->name("contacts.")->group(
-    function () {
-        Route::get("/", [ContactController::class, "index"])->name("index");
-        Route::get("/create", [ContactController::class, "create"])->name("create");
-        Route::post("/", [ContactController::class, "store"])->name("store");
-        Route::get("/{id}", [ContactController::class, "show"])->whereNumber("id")->name("show");
-        Route::get("/{id}/edit", [ContactController::class, "edit"])->where("id", "\d+")->name("edit");
-        Route::put("/{id}", [ContactController::class, "update"])->name("update");
-        Route::delete("/{id}", [ContactController::class, "destroy"])->name("destroy");
-        Route::delete("/{contact}/restore", [ContactController::class, "restore"])->name("restore");
-        Route::delete("/{contact}/force-delete", [ContactController::class, "forceDelete"])->name("force-delete");
-        // Route::resource("/", ContactController::class);
-    }
-);
+Route::middleware(['auth'])->group(function () {
+    Route::get('/dashboard', DashboardController::class)->name("dashboard")/*->middleware(['auth'])*/;
 
-// Optional parameters
-Route::get("/companies/{name?}", function ($company_name = null) {
-    if ($company_name)
-        return "Company " . $company_name;
-    else
-        return "<h1>All companies</h1";
-})
-    // ->where("name", "[a-zA-Z ]+") // use regex for contraining
-    ->whereAlpha("id") // use build-in function for constraining
-    ->name("company_name");
-
-Route::resource("/companies", CompanyController::class);
-
-Route::resources([
-    "/tags" => TagController::class,
-    "/tasks" => TaskController::class
-]);
-
-Route::resource("/activities", ActivityController::class)
-    // ->except()
-    ->only(["create", "store", "index", "show", "update", "destroy"])
-    ->names(
-        [
-            "index" => "activities.all",
-            "show" => "activities.view"
-        ]
+    Route::controller(ContactController::class)->prefix("contacts")->name("contacts.")->group(
+        function () {
+            Route::get("/", [ContactController::class, "index"])->name("index");
+            Route::get("/create", [ContactController::class, "create"])->name("create");
+            Route::post("/", [ContactController::class, "store"])->name("store");
+            Route::get("/{id}", [ContactController::class, "show"])->whereNumber("id")->name("show");
+            Route::get("/{id}/edit", [ContactController::class, "edit"])->where("id", "\d+")->name("edit");
+            Route::put("/{id}", [ContactController::class, "update"])->name("update");
+            Route::delete("/{id}", [ContactController::class, "destroy"])->name("destroy");
+            Route::delete("/{contact}/restore", [ContactController::class, "restore"])->name("restore");
+            Route::delete("/{contact}/force-delete", [ContactController::class, "forceDelete"])->name("force-delete");
+            // Route::resource("/", ContactController::class);
+        }
     );
 
-Route::resource("contacts.notes", ContactNoteController::class)->shallow();
+    // Optional parameters
+    Route::get("/companies/{name?}", function ($company_name = null) {
+        if ($company_name)
+            return "Company " . $company_name;
+        else
+            return "<h1>All companies</h1";
+    })
+        // ->where("name", "[a-zA-Z ]+") // use regex for contraining
+        ->whereAlpha("id") // use build-in function for constraining
+        ->name("company_name");
 
+    Route::resource("/companies", CompanyController::class);
+
+    Route::resources([
+        "/tags" => TagController::class,
+        "/tasks" => TaskController::class
+    ]);
+
+    Route::resource("/activities", ActivityController::class)
+        // ->except()
+        ->only(["create", "store", "index", "show", "update", "destroy"])
+        ->names(
+            [
+                "index" => "activities.all",
+                "show" => "activities.view"
+            ]
+        );
+
+    Route::resource("contacts.notes", ContactNoteController::class)->shallow();
+});
 // Fallback routes
 // Route::fallback(function () {
 //     return "<h1>Sorry, the page does not exist</h1>";
