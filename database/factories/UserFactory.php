04928f37323a54ac3bbbd342fffcd15c5a5a98cc
datlt-vsvn <tandat.luong@veriserve-vietnam.com>
2023-05-17 17:17:18 +0700

    feat: remembering users and signing users out

--- database/factories/UserFactory.php
+++ database/factories/UserFactory.php
@@ -23,7 +23,7 @@ class UserFactory extends Factory
             'email' => fake()->unique()->safeEmail(),
             'email_verified_at' => now(),
             'password' => Hash::make("123456"), // password
-            'remember_token' => Str::random(10),
+            // 'remember_token' => Str::random(10),
         ];
     }
 
